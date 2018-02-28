9b8b6a8f56d76d65f6c281dced355bdf88ca913c
rjthalman <rjthalman@github.com>
2018-02-28 09:02:33 -0800

    lesson 7
    flex box and gallery

---
+++ Assignments/Lesson-8/scripts/WindChill.js
@@ -0,0 +1,13 @@
+function WindChill(){
+   var HighTemp = get("tempHi").value;
+   var LowTemp = get("tempLo").value;
+    var AveTemp = (HighTemp + LowTemp)/2
+   var windChill = 35.74 + (0.6215 * AveTemp) - 35.75 * (Math.pow(0.16, WindSpeed)) + 0.4275 * AveTemp (Math.pow(0.16, WindSpeed))
+ get("output").innerHTML = windChill;
+}
+CSS--var temp1 = parseInt(document.getElementById("tempHi").innerHTML);
+
+HTML--<div class="highlow">
+<p>High: <span id="tempHi">90</span>°F</p>
+<p>Low: <span id="tempLo">66</span>°F</p>
+</div>
