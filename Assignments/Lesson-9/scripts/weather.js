1f9a9c5ffddf398bf65970443630fe38c0b3a788
rjthalman <rjthalman@github.com>
2018-03-13 13:32:41 -0700

    undate

--- Assignments/Lesson-9/scripts/weather.js
+++ Assignments/Lesson-9/scripts/weather.js
@@ -1,34 +1,64 @@
-var weatherObject = new XMLHttpRequest();
 
-weatherObject.open('GET','//api.wunderground.com/api/1a69d9fee0c2aac4/conditions/q/MN/Franklin.json', true);
+                var header = document.querySelector('header');
+                var section = document.querySelector('section');
+                var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+                var request = new XMLHttpRequest();
+                request.open('GET', requestURL);
+                request.responseType = 'json';
+                request.send();
+                request.onload = function() {
+                    var threetowns = request.response;
+                    populateHeader(threetowns);
+                    showTowns(threetowns);
+                }
+                function populateHeader(jsonObj) {
+                    var myH1 = document.createElement('h1');
+                    myH1.textContent = "Your Local Community";
+                    header.appendChild(myH1);
 
-weatherObject.send();
+                    var myPara = document.createElement('p');
+                    myPara.textContent = "Franklin, Greenville, and Springfield ";
+                    header.appendChild(myPara);
+                }
+                function showTowns(jsonObj) {
+                    var towns = jsonObj['towns'];
+                    towns.splice (2,1);
 
-weatherObject.onload = function(){
-    var weatherInfo = JSON.parse(weatherObject.responseText);
-    console.log(weatherInfo);
+                    for (var i = 0; i < towns.length; i++) {
+                        var myArticle = document.createElement('article');
+                        var myH2 = document.createElement('h2');
+                        var myPara1 = document.createElement('p');
+                        var myPara2 = document.createElement('p');
+                        var myPara3 = document.createElement('p');
+                        var myPara4 = document.createElement('p');
+                        var myPara5 = document.createElement('p');
+                        var myList = document.createElement('ul');
+
+                        myH2.textContent = towns[i].name;
+                        myPara1.textContent = 'Motto: ' + towns[i].motto;
+                        myPara2.textContent = 'Founded: ' + towns[i].yearFounded;
+                        myPara3.textContent = 'Current population: ' + towns[i].currentPopulation;
+                        myPara4.textContent = 'Average Rainfall: ' + towns[i].averageRainfall + " inches";
+                        myPara5.textContent = 'Community Events:';
+
+                        var communityEvents = towns[i].events;
+                        for (var j = 0; j < communityEvents.length; j++) {
+                            var listItem = document.createElement('li');
+                            listItem.textContent = communityEvents[j];
+                            myList.appendChild(listItem);
+                        }
+
+                        myArticle.appendChild(myH2);
+                        myArticle.appendChild(myPara1);
+                        myArticle.appendChild(myPara2);
+                        myArticle.appendChild(myPara3);
+                        myArticle.appendChild(myPara4);
+                        myArticle.appendChild(myPara5);
+                        myArticle.appendChild(myList);
+
+                        section.appendChild(myArticle);
+                    }
+                }
+
     
-    document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full
     
-    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f
-    
-    document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url
-}
-
-var townUpDate = new XMLHttpRequest();
-
-townUpDate.open('GET','https://byui-cit230.github.io/weather/data/towndata.json', true);
-
-townUpDate.send();
-
-townUpDate.onload = function(){
-    var townInfo = JSON.parse(weatherObject.responseText);
-    console.log(townInfo);
-    
-    document.getElementById('motto').innerHTML = townInfo.towns.motto
-    
-    document.getElementById('currentTemp').innerHTML = townInfo.current_observation.temp_f
-    
-    document.getElementById('weather_icon').src = townInfo.current_observation.icon_url
-}
-
