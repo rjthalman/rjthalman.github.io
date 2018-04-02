7f8bfcc95f4552107175288a04a35713b0153e73
rjthalman <rjthalman@github.com>
2018-04-02 10:19:02 -0700

    update

--- Assignments/weathersite/scripts/FranklinWeather.js
+++ Assignments/weathersite/scripts/FranklinWeather.js
@@ -5,13 +5,12 @@ weatherObject.open('GET', '//api.wunderground.com/api/1a69d9fee0c2aac4/condition
 
 weatherObject.send();
 
-weatherObject.onload = function() {
+weatherObject.onload = function () {
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
-document.getElementById('tempHi').innerHTML = weatherInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
+    document.getElementById('tempHi').innerHTML = weatherInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit;
     document.getElementById('tempLo').innerHTML = weatherInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit;
     document.getElementById('Temp').innerHTML = weatherInfo.current_observation.temp_f;
     document.getElementById('weather').innerHTML = weatherInfo.current_observation.weather;
-   document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url; 
-    document.getElementById('windSpeed').src = weatherInfo.current_observation.wind_mph; 
-} 
+    document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url; document.getElementById('windSpeed').innerHTML = weatherInfo.current_observation.wind_mph; 
+}
